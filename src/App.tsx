35a360495acee9dadc8ebd3e7d421e11c08092d7
LucasFreitass <lucas_rfreitas@outlook.com.br>
2022-04-25 00:16:04 -0300

    fix - duplicate components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { TransactionsTable } from './components/TransactionsTable';
 import { GlobalStyle } from './styles/global';
 
 
@@ -9,7 +8,6 @@ export function App() {
     <>
       <Header/>
       <Dashboard/>
-      <TransactionsTable/>
       <GlobalStyle/>
     </>
   );
